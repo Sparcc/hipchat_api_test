5a141e6f567344bb2ceeddf482ee68b276934b02
avih8r <sparco.209@gmail.com>
2017-12-08 15:00:41 +1100

    fucking works now what the fuck

--- hipchat_test.py
+++ hipchat_test.py
@@ -4,7 +4,7 @@ import requests
 
 room = {'QA': 'https://rxpservices.hipchat.com/v2/room/4154443',
 	'CCA': 'https://rxpservices.hipchat.com/v2/room/3554226',
-	'TEST': 'http://rxpservices.hipchat.com/v2/room/4216688',
+	'TEST': 'http://api.hipchat.com/v2/room/4216688',
 	'CCA_Notif': 'https://rxpservices.hipchat.com/v2/room/4292319'
 	};
 	
@@ -22,6 +22,7 @@ def getUrl(key, usePersonalToken=True):
 		returnUrl = room[key] +'/notification?auth_token='+ personalToken
 	else:
 		returnUrl = room[key] +'/notification?auth_token='+ roomToken[key]
+	#return room[key]
 	return returnUrl
 	
 def getToken(key, usePersonalToken=True):
@@ -32,18 +33,11 @@ def getToken(key, usePersonalToken=True):
 	return token
 
 
-url = getUrl('TEST',False)
-#url = 'https://rxpservices.hipchat.com/v2/room/4216688/notification?auth_token=vpgCm6XfSX3cLVrHNerPZWWINXCPBgizAV3yIe53'
-#url = 'https://requestb.in/1esu8nr1'
+url = getUrl('TEST')
 f = open('payload.json','r') #read 
 payload = f.read()
-#payload = payload[1:-1]
 headers = {'Content-type': 'application/json'}
-headers['Authorization'] = "Bearer " + getToken('TEST',False)
-#headers['Authorization'] = "Bearer " + personalToken
-#r = requests.post(url, headers=headers, data=payload)
-r = requests.post(url, data=json.dumps(payload), headers=headers)
-#r = requests.post(url, data=payload)
-#r = requests.post(url, data=json.dumps(payload))
+#headers['Authorization'] = "Bearer " + getToken('TEST')
 
+r = requests.post(url, data=json.dumps(payload), headers=headers)
 r.raise_for_status()
